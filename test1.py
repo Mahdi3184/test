701e19f2f9fd03825b0275976c9e35e4e637ddf5
Mahdi3184 <107339587+Mahdi3184@users.noreply.github.com>
2022-06-12 10:27:05 +0430

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,14 @@
+num_1 = input ("Enter a num1 : ")
+operator = input ("please select onn of the following \n[+,-,*,/] : ")
+num_2 = input ("Enter a num2 : ")
+print (f"'{num_1}' {operator} '{num_2}' = " )
+
+print("*" *50)
+
+name = input("Enter a your name : ")
+last_name = input("Enter a your last name : ")
+age = input("Enter a your age : ")
+city = input("Enter a your city : ")
+print("\n")
+my_information = { "name" : name , "last_name" : last_name , "age" : age , "city" : city}
+print (my_information)
