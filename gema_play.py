650b14cbb291fffafc5e3a3731ba8dd3bccddd20
Mahdi3184 <107339587+Mahdi3184@users.noreply.github.com>
2022-07-13 11:44:24 +0430

    Add files via upload

---
+++ gema_play.py
@@ -0,0 +1,51 @@
+player_1_name = input("\nplayer_1 -> enter your name: ")
+player_2_name = input("\nplayer_2 -> enter your name:  ")
+Score_player_1 = 0
+Score_player_2 = 0
+while Score_player_1 < 3 and Score_player_2 < 3: 
+    print("\ntyap 'end' to exit")
+    player_1 = input("\nmake your move  [rock,paper,scissors] player_1: ").lower()
+    player_2 = input("\nmake your move  [rock,paper,scissors] player_2 : ").lower()
+    if player_1 == "end" or player_2 == "end":
+        print("\ngame over")
+    elif player_1 == player_2:
+        print("\nthat's a tie")
+    elif player_1 == "rock" and player_2 == "paper":
+        print(f"\nThe {player_2_name} won")
+        Score_player_2 += 1
+        print(f"\n{player_1_name} is :",Score_player_1)
+        print(f"\n{player_2_name} is :", Score_player_2)
+    elif player_1 == "rock" and player_2 == "scissors":
+        print(f"\nthe {player_1_name} won")
+        Score_player_1 += 1
+        print(f"\n{player_1_name} is :",Score_player_1)
+        print(f"\n{player_2_name} is :", Score_player_2)
+    elif player_1 == "paper" and player_2 == "rock":
+        print(f"\nThe {player_1_name} won")
+        Score_player_1 += 1
+        print(f"\n{player_1_name} is :",Score_player_1)
+        print(f"\n{player_2_name} is :", Score_player_2)
+    elif player_1 == "paper" and player_2 == "scissors":
+        print(f"\nThe {player_2_name} won")
+        Score_player_2 += 1
+        print(f"\n{player_1_name} is :",Score_player_1)
+        print(f"\n{player_2_name} is :", Score_player_2)
+    elif player_1 == "scissors" and player_2 == "rock":
+        print(f"\nThe {player_2_name} won")
+        Score_player_2 += 1
+        print(f"\n{player_1_name} is :",Score_player_1)
+        print(f"\n_{player_2_name} is :", Score_player_2)
+    elif player_1 == "scissors" and player_2 == "paper":
+        print(f"\nThe {player_1_name} won")
+        Score_player_1 += 1
+        print(f"\n{player_1_name} is :",Score_player_1)
+        print(f"\n{player_2_name} is :", Score_player_2)
+    else:
+        print("\nplease enter one of these three")
+if  Score_player_2 <  Score_player_1 :
+    print(f"\nThe {player_1_name} won thes contest")
+elif  Score_player_2 > Score_player_1:
+    print(f"\nThe {player_2_name} won thes contest")
+else:
+    print("\nTher is no  winner in this contest")
+               
